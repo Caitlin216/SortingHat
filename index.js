f67e1b935dd2467dd4f45f41b2e873cc3b579e9b
arisualor <39636857+arisualor@users.noreply.github.com>
2018-06-29 19:52:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -91,9 +91,8 @@ bot.on("message", async message => {
         cooldown.add(message.author.id);
     //}
     let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
+    let cmd = messageArray[0].toLowerCase();
     let args = messageArray.slice(1);
-    cmd.toLowerCase();
     
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
     if(commandfile) commandfile.run(bot,message,args); 
