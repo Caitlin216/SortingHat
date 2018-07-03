5bbc2ed89659477d8a5861012b0c85397dd89d1c
arisualor <39636857+arisualor@users.noreply.github.com>
2018-07-03 23:08:39 +0200

    Update hufflepuff.js

--- commands/hufflepuff.js
+++ commands/hufflepuff.js
@@ -4,10 +4,10 @@ module.exports.run = async (bot, message, args) => {
 
     //let role = args.join(" ");
     //if(!role) return message.reply("Specify a role!");
-    let gRole = message.guild.roles.find(`name`, role);
-    if(!gRole) return message.reply("Couldn't find that role.");
+    let role = message.guild.roles.find(`name`, role);
+    if(!role) return message.reply("Couldn't find that role.");
 
-    await(member.addRole(gRole.id));
+    await(member.addRole(role.id));
     message.reply("Hmm...okay...I can see you are optimistic and kind-hearted...hmm...I know there is a house that can fit in you in...go for HUFFLEPUFF!!!");
 }
 
