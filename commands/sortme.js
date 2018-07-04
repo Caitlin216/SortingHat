579b72425825bce0f92b650611a7e089e3ac0628
arisualor <39636857+arisualor@users.noreply.github.com>
2018-07-04 19:56:52 +0200

    Update sortme.js

--- commands/sortme.js
+++ commands/sortme.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    message.reply("Hmmm...yes...no...you are a tough one! [link](https://my.pottermore.com/sorting)");
+    message.reply("Hmmm...yes...no...you are a tough one! https://my.pottermore.com/sorting");
 }
 
 module.exports.help = {
