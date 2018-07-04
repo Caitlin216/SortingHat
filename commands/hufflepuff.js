27281d29f25e12e41e31acfa2021fc950383f0bf
arisualor <39636857+arisualor@users.noreply.github.com>
2018-07-04 18:50:43 +0200

    Update hufflepuff.js

--- commands/hufflepuff.js
+++ commands/hufflepuff.js
@@ -5,11 +5,11 @@ module.exports.run = async (bot, message, args) => {
     let Role = message.guild.roles.find('name','Hufflepuff')
     message.member.addRole(Role)
 
-    let aRole = message.guild.roles.find('name','Ravenclaw')
+    let aRole = message.guild.roles.find('name','Ravenclaw','Slytherin','Gryffindor')
     message.member.removeRole(aRole)
     .then(deleted => console.log(`Deleted role ${deleted.name}`))
     .catch(console.error)
-
+   
     message.reply("Hmm...okay...I can see you are optimistic and kind-hearted...hmm...I know there is a house that can fit in you in...go for HUFFLEPUFF!!!");
 }
 
