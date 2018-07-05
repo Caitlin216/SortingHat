c7df4cab3b74b815c0dfa976e201710e1e95b5be
arisualor <39636857+arisualor@users.noreply.github.com>
2018-07-05 21:14:05 +0200

    Update roleplayer.js

--- commands/roleplayer.js
+++ commands/roleplayer.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     let Role = message.guild.roles.find('name','Roleplayer')
     message.member.addRole(Role)
 
-    message.reply("You are a wizard, @<member>!");
+    message.reply("You are a wizard, ${member}!");
 }
 
 module.exports.help = {
