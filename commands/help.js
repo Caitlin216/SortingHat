9051864b0e7c3ff56e5bec1a00d5f99c555e6568
arisualor <39636857+arisualor@users.noreply.github.com>
2018-07-10 19:58:15 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
     .setColor("#CD853F")
     .addBlankField(true)
     .addField("**Sorting**", "`!ravenclaw` get sorted in Ravenclaw, `!gryffindor` get sorted in Gryffindor, `!hufflepuff` get sorted in Hufflepuff, `!Slytherin` get sorted in Slytherin, `!sortme` get a link to sorting hat quiz.")
-    .addField("**Add Role**", "`!roleplayer` get the Roleplayer role and see the roleplay channels, `!artist` get the Artist role.")
+    .addField("**Add Role**", "`!roleplayer` get the Roleplayer role and see the roleplay channels, `!artist` get the Artist role, `!dj`get the DJ role, `!oc1` get the Character #1 role, `!oc2` get the Character #2 role, `!oc3` get the Character #3 role, `!oc4` get the Character #4 role.")
     .addField("**Information**","`!botinfo` shows info about the bot, `!serverinfo` shows info about the server, `!help` shows list of commands.")
     .addField("**Incident**","`!report @username <reason>` report the selected user.")
     .addField("**Entertainment**","`!8ball <question>` answers all your questions, `!cat` shows a random picture or gif of a cat, `!doggo` shows a random picture or gif of a dog, `!roll` rolls a dice.")
