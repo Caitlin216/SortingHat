3bf2ca847e9e8cb4f6e1ce0812d8d2bc27b812a8
arisualor <39636857+arisualor@users.noreply.github.com>
2018-07-10 18:10:15 +0200

    Create character1.js

---
+++ commands/character1.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    let Role = message.guild.roles.find('name','Character #1')
+    message.member.addRole(Role)
+
+    let aRole = message.guild.roles.find('name','Character #2')
+    message.member.removeRole(aRole)
+    .then(deleted => console.log(`Deleted role ${deleted.name}`))
+    .catch(console.error)
+    
+    let bRole = message.guild.roles.find('name','Character #3')
+    message.member.removeRole(bRole)
+    .then(deleted => console.log(`Deleted role ${deleted.name}`))
+    .catch(console.error)
+    
+    let cRole = message.guild.roles.find('name','Character #4')
+    message.member.removeRole(cRole)
+    .then(deleted => console.log(`Deleted role ${deleted.name}`))
+    .catch(console.error)
+
+    message.reply("You currently have 1 character in the Role-Play.");
+}
+
+module.exports.help = {
+    name: "character1"
+}
