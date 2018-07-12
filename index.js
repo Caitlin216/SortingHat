77ca529c060e9acb0293233c0be10c21403ed2ae
arisualor <39636857+arisualor@users.noreply.github.com>
2018-07-12 22:22:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const botconfig = require("./botconfig.json");
-//const tokenfile = require("./token.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
@@ -26,13 +25,12 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online!`);  
     bot.user.setActivity("On Hogwarts Mystery!");
-    //bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
 });
 
 bot.on("guildMemberAdd", async member => {
     
     console.log(`${member.id} joined the server!`);
-    let role = member.guild.roles.find('name', 'Student');
+    let role = member.guild.roles.find('name', 'Invited');
     member.addRole(role);
 });
 
@@ -42,27 +40,6 @@ bot.on("guildMemberAdd", async member => {
     let welcomechannel = member.guild.channels.find(`name`, "new-students");
     welcomechannel.send(`Dear Mr/Ms ${member}, we are pleased to inform you that you have been accepted at Hogwarts School of Witchcraft and Wizardry. Please find enclosed a list of all necessary objectives to complete before entering the school. You are required to read the rules at <#448968076038373383>. After reading the rules proceed to <#448969324254855185> to get sorted in a house. Last but not least fill in a short bio about yourself in <#461248739114221589>. Sincerely Professor Albus Dumbledore. https://imgur.com/a/WVLkKQ8`);
 
-});
-
-//bot.on("guildMemberRemove", async member => {
-
-    //console.log(`${member.id} left the server.`);
-    //let welcomechannel = member.guild.channels.find(`name`, "home");
-    //welcomechannel.send(`Good riddance! ${member} has bailed on the server!`);
-//});
-
-bot.on("channelCreate", async channel => {
-
-    console.log(`${channel.name} has been created.`);
-    let sChannel = channel.guild.channels.find(`name`, "common-room");
-    sChannel.send(`${channel} channel has been created!`);
-});
-
-bot.on("channelDelete", async channel => {
-
-    console.log(`${channel.name} has been deleted.`);
-    let sChannel = channel.guild.channels.find(`name`, "common-room");
-    sChannel.send(`${channel.name} channel has been deleted.`);
 });
 
 bot.on("message", (message) => {
@@ -95,9 +72,8 @@ bot.on("message", async message => {
         message.delete();
         return message.reply("You have to wait 5 seconds between commands.")
     }
-    //if(!message.content.hasPermission("ADMINISTRATOR")){
-        cooldown.add(message.author.id);
-    //}
+    
+    cooldown.add(message.author.id);    
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0].toLowerCase();
     let args = messageArray.slice(1);
