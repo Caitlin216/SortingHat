7dc41933a375395bfe114a175216e0c536b6d0e8
arisualor <39636857+arisualor@users.noreply.github.com>
2018-07-14 23:49:35 +0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -11,8 +11,6 @@ module.exports.run = async (bot, message, args) => {
         .addField("Created On", message.guild.createdAt)
         .addField("You Joined", message.member.joinedAt)
         .addField("Total Members", message.guild.memberCount);
-
-        //return message.channel.send(serverembed).then(msg => {msg.delete(10000)});
 }
 
 module.exports.help = {
